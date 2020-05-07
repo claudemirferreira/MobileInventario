209d537cc9dc92059ba1f8e53e8d758f4c3943e6
nelsonsozinho <nelsonsozinho@gmail.com>
2020-05-07 00:20:26 -0400

    wip: start search screen

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Image, Text, TouchableOpacity, Linking} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation, useRoute} from '@react-navigation/native';
@@ -6,21 +6,48 @@ import {useNavigation, useRoute} from '@react-navigation/native';
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
+import api from '../../services/api';
 
 export default function Search() {
+    const [itens, setItens] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [total, setTotal] = useState(0);
+    const [item, setItem] = useState();
+
     const navigation = useNavigation();
     const route = useRoute();
-        
     
     function navigateBack() {
         navigation.goBack();
+    }
+
+    async function loadItens() {
+        if(loading) {
+            return;
+        }
+
+        if(total > 0 && itens.length === total) {
+            return;
+        }
+
+        setLoading(true);
+
+        // const response = await api.get('item', {params: {item}});
+        await api.get('item', {params: {item}}).then(response => {
+            setItems([... itens, ...response.data]); 
+        });
+
+        // setItems([... itens, ...response.data]);
+        //setTotal(response.headers['x-total-count']);
+        //setPage(page+1);
+        setLoading(false);
+
     }
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg}/>
-
                 <TouchableOpacity onPress={navigateBack}>
                     <Feather name='arrow-left' size={28} color='#e02041'/>
                 </TouchableOpacity>
@@ -28,26 +55,8 @@ export default function Search() {
 
             <View style={styles.incident}>
 
-                
-
             </View>
 
-            <View style={styles.contactBox}>
-                <Text style={styles.heroTitle}>Texto 1</Text>
-                <Text style={styles.heroTitle}>Texto 2</Text>
-                <Text style={styles.heroDesctiption}>Texto contato</Text>
-
-                <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} >
-                        <Text style={styles.actionText}>WhatsApp</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.action} >
-                        <Text style={styles.actionText}>E-mail</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </View>
         </View>
     )
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://10.0.1.30:3333/'
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'http://localhost:3000/'
 });
 
 export default api;

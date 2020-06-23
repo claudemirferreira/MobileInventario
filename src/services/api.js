b5d34ea07ec9d2b14533e0b655b0744061894e69
nelnsonsozinho <nelsonsozinho@gmail.com>
2020-06-23 16:00:47 -0400

    server update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './storage-utils'
 
 const api = axios.create({
-    baseURL: 'http://10.0.1.30:3000/'
+    baseURL: 'http://54.145.87.8:3000/'
 });
 
 api.interceptors.request.use(async (config) => {
